465fd1b1d3ef7a1087b6d277c60381d3dafcc3d8
Shayan Tehrani <shayanthrn@gmail.com>
2020-05-13 04:48:06 -0700

    add while true in child processes of test to keep them alive while test

--- getchildrenTest.c
+++ getchildrenTest.c
@@ -8,21 +8,27 @@ main(void)
   int pid1,pid2,pid3;
   pid1=fork();
   if(pid1==0){
-    
+    while(1){
+        
+    }
   }
   else{
       pid2=fork();
       if(pid2==0){
+        while(1){
         
+    }
       }
       else{
           pid3=fork();
           if(pid3==0){
-            
+            while(1){
+                
+            }
           }
           else{
             printf(1,"pid of n1 child is: %d n2 is : %d and n3 is: %d and the output of getchildren is : %d\n",pid1,pid2,pid3,getChildren());
-            
+
           }
       }
   }
