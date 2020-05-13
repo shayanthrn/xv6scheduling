334bd020ac4631b48fe4c2c135f8a31e3f3ff5e0
Shayan Tehrani <shayanthrn@gmail.com>
2020-05-13 04:43:27 -0700

    implemention of getchildrenTest is done

--- getchildrenTest.c
+++ getchildrenTest.c
@@ -5,6 +5,26 @@
 int
 main(void)
 {
-  printf(1, "out put of getchildren is : %d\n",getChildren());
+  int pid1,pid2,pid3;
+  pid1=fork();
+  if(pid1==0){
+    
+  }
+  else{
+      pid2=fork();
+      if(pid2==0){
+        
+      }
+      else{
+          pid3=fork();
+          if(pid3==0){
+            
+          }
+          else{
+            printf(1,"pid of n1 child is: %d n2 is : %d and n3 is: %d and the output of getchildren is : %d\n",pid1,pid2,pid3,getChildren());
+            
+          }
+      }
+  }
   exit();
 }
