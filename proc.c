693c07bdc2a2a5549509acefc8ddd74c7aeff036
Shayan Tehrani <shayanthrn@gmail.com>
2020-05-14 03:41:14 -0700

    changepolicy implemented

--- proc.c
+++ proc.c
@@ -7,6 +7,10 @@
 #include "proc.h"
 #include "spinlock.h"
 
+
+
+int scheduler_select=0;
+
 struct {
   struct spinlock lock;
   struct proc proc[NPROC];
@@ -322,36 +326,41 @@ wait(void)
 void
 scheduler(void)
 {
-  struct proc *p;
-  struct cpu *c = mycpu();
-  c->proc = 0;
+  if(scheduler_select==0){
+    struct proc *p;
+    struct cpu *c = mycpu();
+    c->proc = 0;
   
-  for(;;){
-    // Enable interrupts on this processor.
-    sti();
+    for(;;){
+      // Enable interrupts on this processor.
+      sti();
 
-    // Loop over process table looking for process to run.
-    acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
+     // Loop over process table looking for process to run.
+      acquire(&ptable.lock);
+      for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+       if(p->state != RUNNABLE)
+          continue;
 
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
+       // Switch to chosen process.  It is the process's job
+       // to release ptable.lock and then reacquire it
+       // before jumping back to us.
+        c->proc = p;
+        switchuvm(p);
+       p->state = RUNNING;
 
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
+       swtch(&(c->scheduler), p->context);
+       switchkvm();
 
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
-    }
+       // Process is done running for now.
+       // It should have changed its p->state before coming back.
+       c->proc = 0;
+     }
     release(&ptable.lock);
 
+   }
+  }
+  else{
+      // myscheduler
   }
 }
 
@@ -559,5 +568,11 @@ int getChildren(void){
 }
 
 int changePolicy(void){
+  if(scheduler_select==0){
+    scheduler_select=1;
+  }
+  else{
+    scheduler_select=0;
+  }
   return 0;
 }
