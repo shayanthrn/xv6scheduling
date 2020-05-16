615a981c0868f15fbc3e0f971d62f93e7e21a8b1
Shayan Tehrani <shayanthrn@gmail.com>
2020-05-16 13:25:26 -0700

    implemention of waitchild has done

--- proc.c
+++ proc.c
@@ -649,10 +649,45 @@ void updateTimeproc(void){
 
 int
 waitChild(struct timevars *times){
-  times->creation_time=0;
-  times->running_time=1;
-  times->sleep_time=2;
-  times->termination_time=3;
-  times->waiting_time=4;
-  return 0;
+struct proc *p;
+int havekids, pid;
+struct proc *curproc = myproc();
+acquire(&ptable.lock);
+for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        times->creation_time=p->creation_time;
+        times->running_time=p->running_time;
+        times->sleep_time=p->sleep_time;
+        times->termination_time=p->termination_time;
+        times->waiting_time=p->waiting_time;
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
 }
